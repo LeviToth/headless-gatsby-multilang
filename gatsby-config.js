9f2c92430c1ecf3e4d183c72960268e4c014439c
LeviToth <104467873+LeviToth@users.noreply.github.com>
2022-07-27 09:34:52 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-datocms',
       options: {
-        apiToken: '87cdc55b15aa843368e8df1531e7b4',
+        apiToken: '25ffeca9c612e7c411a9a7458c1bea',
         localeFallbacks: {
           'ar-AE': 'en',
         },
