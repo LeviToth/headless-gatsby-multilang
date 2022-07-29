f7afdea6dbeeda0a26a0cdabebeb32e3ec6bb32a
LeviToth <104467873+LeviToth@users.noreply.github.com>
2022-07-29 08:23:20 +0200

    Update Logo.jsx

--- src/components/Layout/Icons/Logo.jsx
+++ src/components/Layout/Icons/Logo.jsx
@@ -1,12 +1,37 @@
 export const Logo = () => (
-  <svg id="ejSln0EQic31" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" shape-rendering="geometricPrecision" text-rendering="geometricPrecision">
-  <g>
-    <rect width="6" height="13" rx="1" ry="1" transform="translate(5 17)" fill="#6baf92" />
-    <rect width="6" height="17" rx="1" ry="1" transform="translate(21 13)" fill="#6baf92" />
-    <rect width="6" height="24" rx="1" ry="1" transform="translate(13 6)" fill="#6baf92" />
-    <path d="M27,14c.001537-.265803-.102813-.521281-.29-.71L21,17.66L21,29c0,.552285.447715,1,1,1h4c.552285,0,1-.447715,1-1Z" fill="#207567" />
-    <path d="M13,23.77v5.23c0,.552285.447715,1,1,1h4c.552285,0,1-.447715,1-1v-9.81Z" fill="#207567" />
-    <path d="M5.27,29.68c.188472.203223.452835.319108.73.32h4c.552285,0,1-.447715,1-1v-3.7Z" fill="#207567" />
-  </g>
-</svg>
+  <svg
+      xmlns="http://www.w3.org/2000/svg"
+      shapeRendering="geometricPrecision"
+      textRendering="geometricPrecision"
+      viewBox="0 0 32 32"
+    >
+      <rect
+        width="6"
+        height="13"
+        fill="#6baf92"
+        rx="1"
+        ry="1"
+        transform="translate(5 17)"
+      ></rect>
+      <rect
+        width="6"
+        height="17"
+        fill="#6baf92"
+        rx="1"
+        ry="1"
+        transform="translate(21 13)"
+      ></rect>
+      <rect
+        width="6"
+        height="24"
+        fill="#6baf92"
+        rx="1"
+        ry="1"
+        transform="translate(13 6)"
+      ></rect>
+      <path
+        fill="#207567"
+        d="M27 14a1 1 0 00-.29-.71L21 17.66V29a1 1 0 001 1h4a1 1 0 001-1zM13 23.77V29a1 1 0 001 1h4a1 1 0 001-1v-9.81zM5.27 29.68A1 1 0 006 30h4a1 1 0 001-1v-3.7z"
+      ></path>
+    </svg>
 );
