c36f746bcb8a84640d37df40e75fb9efe7e4259a
LeviToth <104467873+LeviToth@users.noreply.github.com>
2022-07-29 09:05:02 +0200

    Update Logo.jsx

--- src/components/Layout/Icons/Logo.jsx
+++ src/components/Layout/Icons/Logo.jsx
@@ -1,14 +1,37 @@
 export const Logo = () => (
-   <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="30"
-    height="30"
-    viewBox="0 0 30 30"
-  >
-    <path
-      d="M823.058,231.951a15,15,0,1,1,15-15,3.838,3.838,0,1,1-7.676,0,7.327,7.327,0,1,0-7.327,7.327q.262,0,.522-.018a12.919,12.919,0,0,0,7.673,5.254A15,15,0,0,1,823.058,231.951Z"
-      transform="translate(-808.058 -201.951)"
-      fill="var(--primaryColor)"
-    />
-  </svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="30"
+      height="30"
+      viewBox="0 0 30 30"
+    >
+      <rect
+        width="6"
+        height="13"
+        fill="#6baf92"
+        rx="1"
+        ry="1"
+        transform="translate(5 17)"
+      ></rect>
+      <rect
+        width="6"
+        height="17"
+        fill="#6baf92"
+        rx="1"
+        ry="1"
+        transform="translate(21 13)"
+      ></rect>
+      <rect
+        width="6"
+        height="24"
+        fill="#6baf92"
+        rx="1"
+        ry="1"
+        transform="translate(13 6)"
+      ></rect>
+      <path
+        fill="#207567"
+        d="M27 14a1 1 0 00-.29-.71L21 17.66V29a1 1 0 001 1h4a1 1 0 001-1zM13 23.77V29a1 1 0 001 1h4a1 1 0 001-1v-9.81zM5.27 29.68A1 1 0 006 30h4a1 1 0 001-1v-3.7z"
+      ></path>
+    </svg>
   );
